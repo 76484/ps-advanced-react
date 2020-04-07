aea056346618cd6fd4936803771188851b3fd315
Steven Smith <steven.76484@gmail.com>
2020-04-07 09:43:17 -0400

    Moving DataApi into a separate package under ./lib/
    Note: Babel-node could understand ES6 module export of DataApi only by
    moving the babel configuration out of root's package.json and into a
    babel.config.js file. See:
    https://app.pluralsight.com/library/courses/reactjs-advanced/discussion#comment-4864595663

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: ["@babel/preset-react", "@babel/preset-env"],
+  plugins: [
+    [
+      "@babel/plugin-proposal-decorators",
+      {
+        legacy: true,
+      },
+    ],
+    "@babel/plugin-proposal-function-sent",
+    "@babel/plugin-proposal-export-namespace-from",
+    "@babel/plugin-proposal-numeric-separator",
+    "@babel/plugin-proposal-throw-expressions",
+    "@babel/plugin-proposal-class-properties",
+  ],
+};
